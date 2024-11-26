e4c0bb0439e4bfa0b5acc7c91c9e47feb2abf01a
Gian132004 <giannicolaiduhan@gmail.com>
2024-11-25 21:18:26 -0800

    suko

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const login = require('../models/loginmodel')
-
+const {
+    createuser,
+} = require('../controllers/logincontroler')
 const router = express.Router();
 
 // GET all workouts
@@ -14,16 +15,7 @@ router.get('/:id', (req, res) => {
 }); 
 
 // POST a new workout'
-router.post('/', async (req, res) => {
-    const { username, password } = req.body;
-
-    try {
-        const newlogin = await login.create({ username, password });
-        res.status(200).json(newlogin);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-});
+router.post('/', createuser)
 
 
 // PATCH to update a specific workout by ID
