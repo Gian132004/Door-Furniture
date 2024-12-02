20f33d41cc39a8bf105a31e32c67430e0268011c
FireFlyDeveloper <saludeskimdev@gmail.com>
2024-12-03 00:42:10 +0800

    forgot

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const nodemailer = require("nodemailer");
 const router = express.Router();
 const Login = require("../models/loginmodel"); // Import login model
 
@@ -18,6 +19,45 @@ router.post("/", async (req, res) => {
   } catch (error) {
     console.error("Error during login:", error);
     res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+router.post("/forgot", async (req, res) => {
+  const { username } = req.body;
+
+  try {
+    const user = await Login.findOne({ username, password });
+
+    if (user) {
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: `${process.env.EMAIL}`,
+          pass: `${process.env.PASSWORD}`,
+        },
+      });
+
+      const mailOptions = {
+        from: `${process.env.EMAIL}`,
+        to: `${username}`,
+        subject: "Sending Email using Node.js",
+        text: "That was easy!",
+      };
+
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error);
+          res.status(500).json({ message: "Sending email error" });
+        } else {
+          console.log("Email sent: " + info.response);
+          res.status(200).json({ message: "Email sent successful" });
+        }
+      });
+    } else {
+      res.status(401).json({ message: "Invalid credentials" });
+    }
+  } catch (e) {
+    res.status(500).json({ message: "Sending email error" });
   }
 });
 
